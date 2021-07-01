3995ff92aa57f1f72b2164cbe6c7349d45a39246
Pravinkumar <pravinkumar26990@gmail.com>
2021-07-01 19:33:16 +0530

    Initial Commit

---
+++ CoreData Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData Demo
+//
+//  Created by Pravinkumar on 01/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
